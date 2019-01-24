5135b7446695e2ff407e040fa493e2d0688dc97b
Emily Casey <ecasey@pivotal.io>
2019-01-23 18:21:40 -0600

    WIP acceptance test
    Signed-off-by: Matthew McNew <mmcnew@pivotal.io>

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+lpass_note() {
+  lpass show --notes "Shared-Build Service/$1"
+}
+
 dir="$(cd "$(dirname "$0")" && pwd)"
+
+concourse_service_account_key=$(lpass_note 'gcp-concourse-service-account-json-key')
+
+cat <(cat $dir/resources.yml && cat $dir/jobs.yml) | pbcopy
 
 fly -t wings set-pipeline \
   -p buildpack \
+  --var concourse-service-account-key-json="$concourse_service_account_key" \
   -c <(cat $dir/resources.yml && cat $dir/jobs.yml)
